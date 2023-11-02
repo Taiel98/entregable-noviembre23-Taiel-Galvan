14791267b805ffc7c1dee533a804ebb35d9a6839
Taiel98 <taielrodrigok@gmail.com>
2023-11-01 22:29:31 -0300

    examen front end 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,64 @@
 function App() {
   //Aqui deberias agregar los estados y los handlers para los inputs
+  const [userNombre, setUserName] = useState("");
+  const [color, setColor] = useState("");
+  const onChangeUserName = (e) => setUserName(e.target.value);
+  const onChangeColor = (e) => setColor(e.target.value);
+    const validarUserNombre = (userNombre) => {
+
+      const withoutSpaces = userNombre.trim();
+    
+      if (wihtoutSpaces.length > 2) {
+      return true;
+      } else {
+      return false;
+      }
+      };
+      const validarColor = (color) => {
+
+        const withoutSpaces = color.trim();
+    
+        if (wihtoutSpaces.length > 5) {
+        return true;
+        } else {
+        return false;
+        }
+        };
+
+        const onSubmitForm = (e) => {
+          e.preventDefault();
+  
+          const isUsernameValid = validarUserNombre(userNombre);
+          const isPasswordValid = validarColor(color);
+          
+          if (!isPasswordValid || !isUsernameValid) {
+          alert("Por favor chequea que la información sea correcta");
+          } else {
+          <Card nombre={userNombre}/>;
+          <Card color={color}/>;
+          }
+          };   
+
+
 
   return (
     <div className="App">
       <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <form onSubmit={onSubmitForm}>{/* aqui deberias escribir tu codigo */}
+      <input
+        type="text"
+placeholder="Nombre de usuario"
+value={userNombre}
+onChange={onChangeUserName}
+/>
+<input
+type="text"
+placeholder="Color"
+value={color}
+onChange={onChangeColor}
+/>
+<button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,11 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card(props) {
   return (
     <div>
-      <h2>Esto es un componente</h2>
+      <h2>Tu nombre es {props.nombre}</h2>
+      <h2>Tu color favorito es {props.color}</h2>
     </div>
   );
 }
